1fbc2571dddd3c7df1566aeb3cf5fb61361c193e
Vishal-33333 <vishal9417719812@gmail.com>
2022-01-22 21:56:57 +0530

    arrays (2)

---
+++ Arrays2.java
@@ -0,0 +1,18 @@
+package array;
+
+public class Arrays2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//2nd method array defining
+				int mark[] = {89,88,86,83};
+				
+				//use for loop
+				for(int i=0; i<4;i++){
+					System.out.println(mark [i] + "Number ");
+				}
+
+	}
+
+}
