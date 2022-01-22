50c9861dd49b261efaf734f12f58b7692bbd0bbf
Vishal-33333 <vishal9417719812@gmail.com>
2022-01-22 21:56:20 +0530

    Array.java (1)

---
+++ Arrays.java
@@ -0,0 +1,26 @@
+		// Print the Number using array 
+
+package array;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+
+		//First method array defining 
+	 	int marks []=new int [4];
+		
+	 	marks[0]=56;
+		marks[1]=56;
+		marks[2]=56;
+		marks[3]=56;
+
+		System.out.println(marks[0]);
+		System.out.println(marks[1]);
+		System.out.println(marks[2]);
+		System.out.println(marks[3]);
+
+		
+		
+	}
+
+}
